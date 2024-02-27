5c2f2a5baa63d244958775ed5d8b2dbad9a73b1f
phildehovre <521153@waes.ac.uk>
2024-02-27 12:10:47 +0000

    Add animation and responsive nav

---
+++ readme.md
@@ -0,0 +1,10 @@
+
+# Readme
+
+
+___
+## Credits:
+
+picture by Photo by David Kanigan: https://www.pexels.com/photo/silhouette-of-a-lonely-tree-during-sunset-15571684/
+
+___
--- script.js
+++ script.js
@@ -8,4 +8,23 @@ document.addEventListener('DOMContentLoaded', function() {
         nav.classList.add('nav--show');
     }, 1000);
 
+    const hamburger = document.querySelector('.hamburger_ctn');
+    
+    if (hamburger) {
+
+        hamburger.addEventListener('click', function(e) {
+            hamburger.classList.toggle('active');
+        })
+        
+        window.addEventListener('click', (e) => {
+            if (!e.target.classList.contains('hamburger_ctn--active') || e.target.classList.contains('bar')) {
+                hamburger.classList.remove('hamburger_ctn--active');
+            }
+        })
+    }
 })
+
+
+
+
+// console.log('hamburger',hamburger);
