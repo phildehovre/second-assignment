b3c0e7d7a3c9c955f85c1cc7acc7b2b875bddd4b
phildehovre <521153@waes.ac.uk>
2024-03-12 14:23:32 +0000

    init

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Hamburger menu class toggling for animation
     const hamburger = document.querySelector('.hamburger_ctn');
+    if (!hamburger) alert('No hamburger found, make sure the navbar script is loaded BEFORE the script.js file')
     if (hamburger)
 {        hamburger.addEventListener('click', function(e) {
             // Class toggling logic
