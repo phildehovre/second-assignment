5c9123d904321ed65f4351424821b00deed7d15f
philippedehovre <me@philippedehovre.com>
2024-03-28 12:08:32 +0000

    Reduce image quality

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -28,25 +28,6 @@ document.addEventListener("DOMContentLoaded", () => {
     </nav>
 `
 
-    //     <ul id="mobile_nav">
-    //         <div class="hamburger_ctn">
-    //             <div class="bar top"></div>
-    //             <div class="bar center"></div>
-    //             <div class="bar bottom"></div>
-    //             <div class="hamburger_content">
-    //                 <li style="--order: 1" class="cta">
-    //                     <a href="today.html">
-    //                     Start your journey</a><i class="fas fa-arrow-right"></i></li>
-    //                 </a></li>
-    //                 <li style="--order: 2"><a href="/">Home</a></li>
-    //                 <li style="--order: 3"><a href="">Features</a></li>
-    //                 <li style="--order: 4"><a href="about.html">About</a></li>
-    //                 <li style="--order: 5"><a href="contact.html">Contact</a></li>
-    //             </div>
-    //         </div>
-    //     </ul>
-    // </nav>
-
     let navContainers = document.querySelectorAll('.nav_placeholder')
     navContainers.forEach(container => {
         container.innerHTML = navbar
