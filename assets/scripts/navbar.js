7c32c7b0fb98373f76f06cea1c9d239935f198b0
philippedehovre <me@philippedehovre.com>
2024-03-28 11:30:17 +0000

    Add logo

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -7,12 +7,13 @@ document.addEventListener("DOMContentLoaded", () => {
         <ul id="desktop_nav">
             <div id="follower"></div>
         </ul>
-        <ul id="mobile_nav">
+
+        <div id="mobile_nav">
             <div class="hamburger_ctn">
                 <div class="bar top"></div>
                 <div class="bar center"></div>
                 <div class="bar bottom"></div>
-                <div class="hamburger_content">
+                <ul class="hamburger_content">
                     <li style="--order: 1" class="cta">
                         <a href="today.html">
                         Start your journey</a><i class="fas fa-arrow-right"></i></li>
@@ -21,10 +22,30 @@ document.addEventListener("DOMContentLoaded", () => {
                     <li style="--order: 3"><a href="">Features</a></li>
                     <li style="--order: 4"><a href="about.html">About</a></li>
                     <li style="--order: 5"><a href="contact.html">Contact</a></li>
-                </div>
+                </ul>
             </div>
-    </ul>
-</nav>`
+        </div>
+    </nav>
+`
+
+    //     <ul id="mobile_nav">
+    //         <div class="hamburger_ctn">
+    //             <div class="bar top"></div>
+    //             <div class="bar center"></div>
+    //             <div class="bar bottom"></div>
+    //             <div class="hamburger_content">
+    //                 <li style="--order: 1" class="cta">
+    //                     <a href="today.html">
+    //                     Start your journey</a><i class="fas fa-arrow-right"></i></li>
+    //                 </a></li>
+    //                 <li style="--order: 2"><a href="/">Home</a></li>
+    //                 <li style="--order: 3"><a href="">Features</a></li>
+    //                 <li style="--order: 4"><a href="about.html">About</a></li>
+    //                 <li style="--order: 5"><a href="contact.html">Contact</a></li>
+    //             </div>
+    //         </div>
+    //     </ul>
+    // </nav>
 
     let navContainers = document.querySelectorAll('.nav_placeholder')
     navContainers.forEach(container => {
