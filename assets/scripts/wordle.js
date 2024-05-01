f78c062faac3f53e7d7ec8555441d7fff4c110e0
philippedehovre <ph.dehovre@gmail.com>
2024-05-01 14:34:37 +0100

    Game is working - No spellcheck

--- assets/scripts/wordle.js
+++ assets/scripts/wordle.js
@@ -1,54 +1,20 @@
-function setNewWord() {
-    fetch('https://random-word-api.herokuapp.com/word?length=5')
-        .then((res) => res.json())
-        .then(res => CookieUtils.setCookie(`wotd`, res[0]))
-}
+/**
+ * The functions are at the bottom,
+ * I am taking advantage of hoisting
+ * for clarity of code.
+ * The function names chould be self explanatory
+ */
 
-function getNewWord() {
-    return CookieUtils.getCookie('wotd')
-}
-
-function checkWordandAnimate(wotd, typed) {
-    /**
-     * This function will return two arrays of
-     * indices which can then be used to select
-     * the cells that need updating.
-     */
-    let correct = []
-    let existing = []
-    if (!wotd) return;
-
-    const hash = wotd.split('').reduce((acc, letter) => {
-        acc[letter]? acc[letter] += 1 : acc[letter] = 1
-        return acc
-    }, {})
-
-    const attempt = typed.split('')
-    const comparator = Object.keys(hash)
-    
-    for (let i = 0; i < attempt.length ; i++) {
-        console.log(i)
-        console.log(attempt[i], comparator[i])
-        if (attempt[i] === comparator[i]) {
-            hash[attempt[i]] -= 1
-            correct.push(i)
-        }
-        if (comparator.includes(attempt[i]) && hash[attempt[i]] != 0) {
-            existing.push(i)
-        }
-    }   
-    return [correct, existing]
-}
-
+// Example usage:
 
 document.addEventListener('DOMContentLoaded', () => {
-    checkWordandAnimate()
     let attempts = [];
     let typed = '';
     let gameOver = false
     const rows = document.querySelectorAll('.grid_row');
     const currentRow = rows[attempts.length];
-    let cells = currentRow.querySelectorAll('.cell');
+    let cells = currentRow.querySelectorAll('.cell'); 
+
 
 
     // =========== Initialize WOTD =============
@@ -59,6 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
     Finally, cancelling the interval on a positive check.
     */
     let wotd = CookieUtils.getCookie("wotd")
+
     const interval = setInterval(() => {
     if (wotd.length == 0) {
         setNewWord()
@@ -69,8 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // =========== Registering keystrokes =============
     document.addEventListener('keydown', (e) => {
-        // Allow for backspace functionality
-        
+        // Allow for backspace functionality       
         if (e.key == "Backspace") {
             typed = typed.slice(0, typed.length-1)
             cells[typed.length].textContent = ''
@@ -81,35 +47,67 @@ document.addEventListener('DOMContentLoaded', () => {
                 cells[typed.length - 1].textContent = e.key
             }
         } else if (e.key == "Enter" && typed.length == 5) {
-            const [correct, existing] = checkWordandAnimate(wotd, typed)
-
-            // assigning classes to correct/existing letters.
-            correct.forEach((index) => {
+            const [correct, existing, checked] = checkWordandAnimate(wotd, typed, gameOver)
+            
+                // assigning classes to correct/existing letters.
+                correct.forEach((index) => {
                 cells[index].classList.add('correct')
             })
-            existing.forEach((index) => {
-                cells[index].classList.add('existing')
-            })
-            attempts.push(typed)
-            typed = ''
-            const currentRow = rows[attempts.length]
-            cells = currentRow.querySelectorAll('.cell')
+                existing.forEach((index) => {
+                    cells[index].classList.add('existing')
+                })
+                attempts.push(typed)
+                typed = ''
+                const currentRow = rows[attempts.length]
+                cells = currentRow.querySelectorAll('.cell')
         }
     })
-
-    
-    // =========== Rendering =============
-    // ==== 1. Grid =====
-    const grid = document.querySelector('#grid')
-
-    
-    
-    // ==== 2. Cells =====
-
-    cells.forEach((cell, index) => {
-        
-    })
-    
-    
-
 })
+
+
+function setNewWord() {
+    fetch('https://random-word-api.herokuapp.com/word?length=5')
+        .then((res) => res.json())
+        .then(res => CookieUtils.setCookie(`wotd`, res[0]))
+}
+
+function getNewWord() {
+    return CookieUtils.getCookie('wotd')
+}
+
+function checkWordandAnimate(wotd, typed, gameover) {
+    /**
+     * This function will return two arrays of
+     * indices which can then be used to select
+     * the cells that need updating.
+     */
+    let correct = []
+    let existing = []
+    let checked
+    if (!wotd) return;
+
+
+    const hash = wotd.split('').reduce((acc, letter) => {
+        acc[letter]? acc[letter] += 1 : acc[letter] = 1
+        return acc
+    }, {})
+
+    const attempt = typed.split('')
+    const comparator = Object.keys(hash)
+    
+    for (let i = 0; i < attempt.length ; i++) {
+        if (attempt[i] === comparator[i]) {
+            hash[attempt[i]] -= 1
+            correct.push(i)
+        }
+        if (comparator.includes(attempt[i]) && hash[attempt[i]] != 0) {
+            existing.push(i)
+        }
+    }   
+
+    if (correct.length == 5) {
+        gameOver=true
+    }
+
+    return [correct, existing, checked]
+}
