955d8bb4b782ee99eb562e12feffbb723ff35418
philippedehovre <ph.dehovre@gmail.com>
2024-05-01 13:07:47 +0100

    Game is working

--- assets/scripts/wordle.js
+++ assets/scripts/wordle.js
@@ -1,19 +1,51 @@
 function setNewWord() {
-    const word = fetch('https://random-word-api.herokuapp.com/word?length=5')
+    fetch('https://random-word-api.herokuapp.com/word?length=5')
         .then((res) => res.json())
         .then(res => CookieUtils.setCookie(`wotd`, res[0]))
-        .then(() => CookieUtils.getCookie("wotd"))
 }
 
 function getNewWord() {
     return CookieUtils.getCookie('wotd')
 }
 
+function checkWordandAnimate(wotd, typed) {
+    /**
+     * This function will return two arrays of
+     * indices which can then be used to select
+     * the cells that need updating.
+     */
+    let correct = []
+    let existing = []
+    if (!wotd) return;
+
+    const hash = wotd.split('').reduce((acc, letter) => {
+        acc[letter]? acc[letter] += 1 : acc[letter] = 1
+        return acc
+    }, {})
+
+    const attempt = typed.split('')
+    const comparator = Object.keys(hash)
+    
+    for (let i = 0; i < attempt.length ; i++) {
+        console.log(i)
+        console.log(attempt[i], comparator[i])
+        if (attempt[i] === comparator[i]) {
+            hash[attempt[i]] -= 1
+            correct.push(i)
+        }
+        if (comparator.includes(attempt[i]) && hash[attempt[i]] != 0) {
+            existing.push(i)
+        }
+    }   
+    return [correct, existing]
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
+    checkWordandAnimate()
     let attempts = [];
     let typed = '';
-    let gameOver = False
+    let gameOver = false
     const rows = document.querySelectorAll('.grid_row');
     const currentRow = rows[attempts.length];
     let cells = currentRow.querySelectorAll('.cell');
@@ -49,6 +81,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 cells[typed.length - 1].textContent = e.key
             }
         } else if (e.key == "Enter" && typed.length == 5) {
+            const [correct, existing] = checkWordandAnimate(wotd, typed)
+
+            // assigning classes to correct/existing letters.
+            correct.forEach((index) => {
+                cells[index].classList.add('correct')
+            })
+            existing.forEach((index) => {
+                cells[index].classList.add('existing')
+            })
             attempts.push(typed)
             typed = ''
             const currentRow = rows[attempts.length]
